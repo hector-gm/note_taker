21f3890f754f9c335503df5981f8062a5ce4276d
Hector Garcia Millares <hgarcia.millares@gmail.com>
2020-12-16 00:50:03 -0500

    Updating server.js with short-unique-id npm

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 // Declare dependecies
 const express = require('express');
+const uid = require('short-unique-id');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
+const uid = ShortUniqueId();
+
 const PORT = process.env.PORT || 3000;  // ***NECESSARY*** variable value along with PORT specification to run the app in Heroku, avoids error R10
 
 // Set Express to handle data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// Define routes for the directory paths
+// Define routes for the directory paths to use HTML files
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/public/index.html'))
@@ -22,15 +25,23 @@ app.get('/notes', function(req,res){
 
 
 // Link to the database file in JSON format to be read to show notes created
-fs.readFile('db/db.json','utf8', (error,data)=>{
-    if(error)throw error;
-
-    const notes = JSON.parse(data);
+let notes = JSON.parse(fs.readFileSync('/db/db.json','utf8'));
 
 // Link to the notes.html code to be loaded along with the JSON 
 
-    app.get('/api/notes', function(req,res){
+app.get('/api/notes', function(req,res){
         res.JSON(notes);
+    });
+
+app.post('/api/notes', (req,res)=>{
+    const newNote = req.body;
+    new uid;
+
+    notes.push(newNote);
+
+    fs.writeFile('/db/db.json', JSON.stringify(notes),'utf8',function(err){
+        if(err)throw err;
+        res.json(newNote);
     });
 });
 
